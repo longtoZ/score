82974de213ad2c2c5d40a579533becf82c501de5
Nautilus <thanhlongpython@gmail.com>
2022-06-04 08:56:59 +0700

    home_page is being developed

--- search_page/index.php
+++ search_page/index.php
@@ -3,11 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css" media="all">
+	<link rel="stylesheet" type="text/css" href="../expand/css/printing.css" media="print">
 	<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
 	<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-brands/css/uicons-brands.css'>
-	<!-- <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'> -->
-	<title>Tra điểm</title>
 </head>
 
 <body>
