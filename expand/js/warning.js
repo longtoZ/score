a0df2cbb008b3517ca82035af084eb689de1f265
Nautilus <thanhlongpython@gmail.com>
2022-08-26 11:33:18 +0700

    source code migration

--- expand/js/warning.js
+++
@@ -1,2 +0,0 @@
-console.error("%c Stop ruining my website...!", "font-size: 2em")
-console.log("%c Stop it! Score is merely a small project built by a student, so performance and security are very poor. If you came here just to explore this mess of code then... go ahead. And if you want to DDOS the system then just donttttt! Let me pass this year and then do whatever you want.", "color:white; font-size: 1.1em")
